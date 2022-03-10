1b554f9568a34ce5b87bbf6edff04b77fa04b3d3
pedro-rodriguez-1995 <rodriguezjimenez1995@gmail.com>
2022-03-10 21:12:37 +0100

    dao de movimiento y test de integracion implementados closes #15

--- src/main/java/com/sinensia/dao/MovimientoDao.java
+++ src/main/java/com/sinensia/dao/MovimientoDao.java
@@ -1,47 +1,256 @@
 package com.sinensia.dao;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.sinensia.contract.IAdd;
+import com.sinensia.contract.IGetById;
 import com.sinensia.contract.IGetByUserId;
 import com.sinensia.contract.IGetByUserIdAndCategoryId;
 import com.sinensia.contract.IRemove;
 import com.sinensia.contract.IModify;
-
 import com.sinensia.model.Movimiento;
+
 public class MovimientoDao extends BaseDao implements IAdd<Movimiento>, IGetByUserId<Movimiento>, IRemove<Movimiento>,
-IModify<Movimiento>, IGetByUserIdAndCategoryId<Movimiento> {
+		IModify<Movimiento>, IGetByUserIdAndCategoryId<Movimiento>, IGetById<Movimiento> {
+
+	private Connection connect;
 
 	@Override
-	public int modify(Movimiento modelo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int modify(Movimiento movimiento) throws SQLException {
+		int rows = 0;
+		PreparedStatement preparedStatement = null;
+		ResultSet rsKey = null;
+		try {
+			connect = super.getConnection();
+			preparedStatement = connect
+					.prepareStatement("UPDATE movimientos SET tipo = ?, importe = ? , fecha = ? WHERE id = ?");
+
+			preparedStatement.setString(1, movimiento.getTipo());
+			preparedStatement.setString(2, movimiento.getImporte().toString());
+			preparedStatement.setString(3, movimiento.getFecha().toString());
+			preparedStatement.setString(4, Integer.toString(movimiento.getIdmovimiento()));
+
+			rows = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (rsKey != null) {
+				rsKey.close();
+
+			}
+			if (connect != null) {
+				connect.close();
+			}
+
+		}
+
+		return rows;
 	}
 
 	@Override
 	public int remove(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		int row = 0;
+		PreparedStatement preparedStatement = null;
+		ResultSet rsKey = null;
+		try {
+			connect = super.getConnection();
+			preparedStatement = connect.prepareStatement("DELETE FROM movimientos WHERE id = ?");
+
+			preparedStatement.setString(1, Integer.toString(id));
+
+			row = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (rsKey != null) {
+				rsKey.close();
+
+			}
+			if (connect != null) {
+				connect.close();
+			}
+
+		}
+
+		return row;
 	}
 
 	@Override
 	public List<Movimiento> getByUserId(int userid) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Movimiento> listamovimientos = new ArrayList<Movimiento>();
+
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connect = super.getConnection();
+			preparedStatement = connect.prepareStatement("SELECT * FROM movimientos WHERE idUsuario = ?");
+			preparedStatement.setString(1, Integer.toString(userid));
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+
+				int idmovimiento = resultSet.getInt("id");
+				int idusuario = resultSet.getInt("idUsuario");
+				int idcategoria = resultSet.getInt("idcategoria");
+				String tipo = resultSet.getString("tipo");
+				BigDecimal importe = resultSet.getBigDecimal("importe");
+				LocalDate fecha = LocalDate.parse(resultSet.getString("fecha"));
+
+				Movimiento movimiento = new Movimiento(idmovimiento, idusuario, importe, idcategoria, tipo, fecha);
+
+				listamovimientos.add(movimiento);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		}
+		if (connect != null) {
+			connect.close();
+		}
+		return listamovimientos;
 	}
 
 	@Override
-	public int add(Movimiento modelo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int add(Movimiento movimiento) throws SQLException {
+		int idmovimiento = 0;
+		PreparedStatement preparedStatement = null;
+		ResultSet rsKey = null;
+		try {
+			connect = super.getConnection();
+			preparedStatement = connect.prepareStatement(
+					"INSERT INTO movimientos (idUsuario,idCategoria,tipo,importe,fecha) VALUE (?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			preparedStatement.setString(1, Integer.toString(movimiento.getIdusuario()));
+			preparedStatement.setString(2, Integer.toString(movimiento.getIdcategoria()));
+			preparedStatement.setString(3, movimiento.getTipo());
+			preparedStatement.setString(4, movimiento.getImporte().toString());
+			preparedStatement.setString(5, movimiento.getFecha().toString());
+
+			preparedStatement.executeUpdate();
+			rsKey = preparedStatement.getGeneratedKeys();
+
+			rsKey.next();
+			idmovimiento = rsKey.getInt(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+			if (rsKey != null) {
+				rsKey.close();
+
+			}
+			if (connect != null) {
+				connect.close();
+			}
+
+		}
+
+		return idmovimiento;
 	}
 
 	@Override
-	public List<Movimiento> getByUserIdAndCategoryId(int userid) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Movimiento> getByUserIdAndCategoryId(int userid, int categoriaid) throws SQLException {
+		List<Movimiento> listamovimientos = new ArrayList<Movimiento>();
+
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connect = super.getConnection();
+			preparedStatement = connect
+					.prepareStatement("SELECT * FROM movimientos WHERE idUsuario = ? AND idCategoria = ?");
+			preparedStatement.setString(1, Integer.toString(userid));
+			preparedStatement.setString(2, Integer.toString(categoriaid));
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+
+				int idmovimiento = resultSet.getInt("id");
+				int idusuario = resultSet.getInt("idUsuario");
+				int idcategoria = resultSet.getInt("idcategoria");
+				String tipo = resultSet.getString("tipo");
+				BigDecimal importe = resultSet.getBigDecimal("importe");
+				LocalDate fecha = LocalDate.parse(resultSet.getString("fecha"));
+
+				Movimiento movimiento = new Movimiento(idmovimiento, idusuario, importe, idcategoria, tipo, fecha);
+
+				listamovimientos.add(movimiento);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		}
+		if (connect != null) {
+			connect.close();
+		}
+		return listamovimientos;
 	}
 
-	
+	@Override
+	public Movimiento getById(int id) throws SQLException {
+		Movimiento movimiento = null;
+
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connect = super.getConnection();
+			preparedStatement = connect.prepareStatement("SELECT * FROM movimientos WHERE id = ?");
+
+			preparedStatement.setString(1, Integer.toString(id));
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				int idmovimiento = resultSet.getInt("id");
+				int idusuario = resultSet.getInt("idUsuario");
+				int idcategoria = resultSet.getInt("idcategoria");
+				String tipo = resultSet.getString("tipo");
+				BigDecimal importe = resultSet.getBigDecimal("importe");
+				LocalDate fecha = LocalDate.parse(resultSet.getString("fecha"));
+
+				movimiento = new Movimiento(idmovimiento, idusuario, importe, idcategoria, tipo, fecha);
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+		}
+		if (connect != null) {
+			connect.close();
+		}
+		return movimiento;
+	}
 
 }
--- src/main/java/com/sinensia/dao/UsuarioDao.java
+++ src/main/java/com/sinensia/dao/UsuarioDao.java
@@ -1,16 +1,49 @@
 package com.sinensia.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.sinensia.contract.IAdd;
 import com.sinensia.contract.IGetById;
 import com.sinensia.model.Usuario;
 public class UsuarioDao extends BaseDao implements IAdd<Usuario> , IGetById<Usuario> {
-
+ private Connection connection;
 	@Override
-	public int add(Usuario modelo) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public int add(Usuario usuario) throws SQLException {
+
+        int idUsuario = 0;
+        PreparedStatement preparedStatement = null;
+        ResultSet rsKey = null;
+        try {
+            connection = super.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO usuario(id, nombre, password) VALUE (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, usuario.getIdusuario());
+            preparedStatement.setString(2, usuario.getNombre());
+            preparedStatement.setString(3, usuario.getPassword());
+
+            preparedStatement.executeUpdate();
+
+            rsKey = preparedStatement.getGeneratedKeys();
+            rsKey.next();
+            idUsuario= rsKey.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (preparedStatement != null)
+                preparedStatement.close();
+            if (rsKey != null)
+                rsKey.close();
+            if (connection != null)
+                connection.close();
+        }
+        return idUsuario;
+    }
 
 	@Override
 	public Usuario getById(int id) throws SQLException {
